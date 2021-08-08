69ea7fcbcc2e2f4c5de6efbc12bfe0a24c618b5b
moustafatammam <moust.tammam@gmail.com>
2021-08-08 10:56:35 +0200

    Handle retrofit api calls error and clear disposables in the viewmodel onClear method

--- app/src/main/java/com/example/moviemaster/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/moviemaster/viewmodel/BaseViewModel.kt
@@ -3,10 +3,14 @@ package com.example.moviemaster.viewmodel
 import androidx.databinding.Observable
 import androidx.databinding.PropertyChangeRegistry
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 open class BaseViewModel: ViewModel(), Observable {
 
     private val callbacks: PropertyChangeRegistry = PropertyChangeRegistry()
+    private val compositeDisposable = CompositeDisposable()
+
 
     override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
         callbacks.add(callback)
@@ -20,5 +24,13 @@ open class BaseViewModel: ViewModel(), Observable {
 
     fun notifyPropertyChanged(fieldId: Int) {
         callbacks.notifyCallbacks(this, fieldId, null)
+    }
+
+    fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
     }
 }
